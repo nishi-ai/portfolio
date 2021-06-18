b69d81d5dcdb70b0ab50ed431bb05d70bb533bd2
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-06-18 22:05:23 +0200

    learning state and props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react'
 import Table from './components/Table'
+import MyComponent from './MyComponent'
 
 class App extends Component {
   render() {
@@ -23,11 +24,31 @@ class App extends Component {
         job: 'Bartender',
       },
     ]
-
+    const characters2 = [
+      {
+        name: 'Charlie2222222',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+  
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <MyComponent shouldsay="from dad" />
+          <MyComponent shouldsay="from mom" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
             hi
@@ -45,6 +66,7 @@ class App extends Component {
         
           <div className="container">
             <Table characterData={characters}/>
+            <Table characterData={characters2}/>
           </div>
         </header>
       </div>
@@ -52,4 +74,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export {App};
