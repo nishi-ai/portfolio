82b518e28a14be6d3be953fa4cf4c332abc1a659
nishi-ai <aiaimforworld@gmail.com>
2022-11-03 07:35:11 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
 import './App.css';
 import React, {Component} from 'react';
 import About from './components/About';
-import Header from './components/Header';
+import Profile from './components/Profile';
 import Portfolio from './components/Portfolio';
+import Header from './components/Header';
 
-//files for understanding components, state and props
-// import Table from './components/Table';
-// import MyComponent from './MyComponent';
-
-// import data from './resumeData.json';
 const data = require('./resumeData.json');
 
 class App extends Component {
   render() {
-    // fetch json or use "require"
-    // const getResumeData = () => {
-    //   fetch('resumeData.json')
-    //   .then(function(resumeData){
-    //     console.log("-------", resumeData);
-    //     return resumeData.json();
-    //   }).then(function(myjson){
-    //     console.log(myjson)
-    //   })
-    // }
-    // getResumeData();
-  
     return (
-      <div className="App">
-        <header className="App-header">
+        <div className="App">
           <Header data={data.main}/>
-          <About data={data.main}/>
-          <Portfolio data={data.portfolio}/>
-        </header>
-      </div>
+          <header className="App-header">
+            <Profile data={data.main}/>
+            <About data={data.main}/>
+            <Portfolio data={data.portfolio}/>
+          </header>
+        </div>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,22 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+const Header = (props) => {
 
-class Header extends Component {
-    render() {
-  
-      if(this.props.data){
-        var name = this.props.data.name;
-        var occupation= this.props.data.occupation;
-        var profilePic = "./images/"+this.props.data.image;
-        var city= this.props.data.city;
-        var networks= this.props.data.social.map(function(network){
-          return <li key={network.name}><a href={network.url}><FontAwesomeIcon icon={network.className} /></a></li>
-        })
-      }
-  
-      return (
-        <header id="home">
-  
-        <div className="row banner">
-           <div className="banner-text">
-                <img className="profile-pic"  src={profilePic} alt="" />
-                <h2 className="responsive-headline">Hello! I'm {name}.</h2>
-                <h3>A {city} based <span>{occupation}</span></h3>
-                <hr />
-                <ul className="social">
-                    {networks}
+    const headerLogo = "./images/"+props.data.header_logo;
+
+    return (
+        <div className="container-header">
+            <header className="logo">
+                <img src={headerLogo} alt="" className="logo-img"/>
+                <nav>
+                <ul>
+                    <li><a href="/">About me</a></li>
+                    <li><a href="/">Portfolio</a></li>
+                    <li><a href="/">Contact</a></li>
                 </ul>
-           </div>
+                </nav>
+            </header>
         </div>
-  
-       
-  
-        </header>
-      );
-    }
-  }
-  
-  export default Header;
-  
+
+    );
+}
+
+export default Header;
